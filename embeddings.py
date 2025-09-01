ba58d0a55a3c1840480b94c7c65556318231419a
PallaviPYXEDA <149144280+PallaviPYXEDA@users.noreply.github.com>
2025-09-01 07:49:09 +0530

    Add files via upload

---
+++ embeddings.py
@@ -0,0 +1,20 @@
+from openai import OpenAI
+import streamlit as st
+import os
+
+
+os.environ["OPENAI_API_KEY"] = st.secrets["OPENAI_API_KEY"]
+TEXT_MODEL = "text-embedding-3-large"
+
+# create client
+client = OpenAI(api_key=os.environ["OPENAI_API_KEY"])
+
+
+def get_openai_embeddings(text: str) -> list[float]:
+    response = client.embeddings.create(
+        input=f"{text}",
+        model=TEXT_MODEL,
+        dimensions=256
+        )
+
+    return response.data[0].embedding
